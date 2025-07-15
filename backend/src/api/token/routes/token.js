7f8abfcb4af2f0c8c63fe7e89ea9986135f91409
Herocku2 <herocku2@github.com>
2025-07-15 14:09:14 -0500

    Arreglar problemas de configuración: JWT secret de PostgREST y simplificar schema de Token

--- backend/src/api/token/routes/token.js
+++ backend/src/api/token/routes/token.js
@@ -6,45 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-const defaultRouter = createCoreRouter('api::token.token');
-
-// Rutas personalizadas
-const customRoutes = {
-  routes: [
-    {
-      method: 'GET',
-      path: '/tokens/lanzados',
-      handler: 'token.findLanzados',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'GET',
-      path: '/tokens/proximos',
-      handler: 'token.findProximos',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'GET',
-      path: '/tokens/candidatos',
-      handler: 'token.findCandidatos',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-  ],
-};
-
-// Combinar rutas por defecto con rutas personalizadas
-module.exports = {
-  routes: [
-    ...defaultRouter.routes,
-    ...customRoutes.routes,
-  ],
-};
+module.exports = createCoreRouter('api::token.token');
